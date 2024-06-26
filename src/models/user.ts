7ffcc0cf45d11efac8784c55f63e3473f5574231
Haoming Wu <104544257+HomingWU@users.noreply.github.com>
2024-06-26 17:53:47 -0400

    user and init spec

---
+++ src/models/user.ts
@@ -0,0 +1,70 @@
+import Client from '../database'
+
+export type User = {
+    id: number;
+    firstName: string;
+    lastName: string;
+    password: string;
+}
+
+export class UserStore {
+    async index(): Promise<User[]> {
+        try {
+            const conn = await Client.connect()
+            const sql = 'SELECT * FROM users'
+
+            const result = await conn.query(sql)
+
+            conn.release()
+
+            return result.rows
+        } catch (err) {
+            throw new Error(`Cannot get users: ${err}`)
+        }
+    }
+
+    async show(id: string): Promise<User> {
+        try {
+            const conn = await Client.connect()
+            const sql = 'SELECT * FROM users WHERE id=($1)'
+
+            const result = await conn.query(sql, [id])
+
+            conn.release()
+
+            return result.rows[0]
+        } catch (err) {
+            throw new Error(`Cannot get user: ${err}`)
+        }
+    }
+
+    async create(u: User): Promise<User> {
+        try {
+            const conn = await Client.connect()
+            const sql = 'INSERT INTO users (firstName, lastName, password) VALUES($1, $2, $3) RETURNING *'
+
+            const result = await conn.query(sql, [u.firstName, u.lastName, u.password])
+
+            conn.release()
+
+            return result.rows[0]
+        } catch (err) {
+            throw new Error(`Cannot create user: ${err}`)
+        }
+    }
+
+    async delete(id: string): Promise<User> {
+        try {
+            const conn = await Client.connect()
+            const sql = 'DELETE FROM users WHERE id=($1) RETURNING *'
+
+            const result = await conn.query(sql, [id])
+
+            conn.release()
+
+            return result.rows[0]
+        } catch (err) {
+            throw new Error(`Cannot delete user: ${err}`)
+        }
+    }
+}
---
+++ src/tests/models/product_spec.ts
@@ -0,0 +1,21 @@
+import {Product, ProductStore} from '../../models/product'
+
+const store = new ProductStore()
+
+describe('Product Model', () => {
+    it('should have an index method', () => {
+        expect(store.index).toBeDefined()
+    })
+
+    it('should have a show method', () => {
+        expect(store.show).toBeDefined()
+    })
+
+    it('should have a create method', () => {
+        expect(store.create).toBeDefined()
+    })
+
+    it('should have a delete method', () => {
+        expect(store.delete).toBeDefined()
+    })
+})
